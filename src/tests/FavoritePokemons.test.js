5e9810a2fa72ef2c11fbe892cc261691805bd04d
Kevillin Santos <kevillin_santos@hotmail.com>
2022-08-16 19:08:33 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa componente Favorite Pokemons', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const favoriteNotFound = screen.getByText('No favorite pokemon found');
+    expect(favoriteNotFound).toBeInTheDocument();
+  });
+});
