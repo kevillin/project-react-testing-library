74d67750299cea7a1a3e9ef664665461077cd2ef
Kevillin Santos <kevillin_santos@hotmail.com>
2022-08-16 19:15:13 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/erro');
+    const ErrorLink = screen.getByRole('heading', { level: 2, name: /not found/i });
+    expect(ErrorLink).toBeInTheDocument();
+
+    const image = screen.getByAltText(/Pikachu crying because the/i);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image).toHaveAttribute('src', url);
+  });
+});
